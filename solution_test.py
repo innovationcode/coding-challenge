202dc7cb1c7c150270d5fc8f231a15eb4990627d
lajawanti <lajawanti.dhake@gmail.com>
2020-12-02 22:07:27 -0500

    test case

---
+++ solution_test.py
@@ -0,0 +1,22 @@
+import unittest
+
+from  Solution import get_days_of_power
+
+class TestSum(unittest.TestCase):
+      def test_1(self): 
+            self.assertEqual(get_days_of_power(3000, 3, 500, 10, 1500, 7, 700000), 141) 
+            self.assertEqual(get_days_of_power(500, 3, 500, 10, 500, 7, 21000), 17)
+            self.assertEqual(get_days_of_power(1300, 0, 500, 0, 1500, 7, 10000), 5)
+            self.assertEqual(get_days_of_power(10000, 3, 500, 10, 1500, 7, 11000), 1)
+
+            self.assertEqual(get_days_of_power(1000, 2, 1000, 6, 1000, 6, 3000), 3)
+            self.assertEqual(get_days_of_power(1000, 2 , 1000, 6, 1000, 6, 2999), 2)
+            self.assertEqual(get_days_of_power(1000, 2, 1000, 6, 1000, 6, 0), 0)  
+            self.assertEqual(get_days_of_power(0, 0, 0, 0, 0, 0, 2000), 0)
+            self.assertEqual(get_days_of_power(1000, 0, 1000, 0, 1000, 0, 5000), 0)
+            self.assertEqual(get_days_of_power(-1000, 2, 1000, 6, 1000, 6, 3000), -1)
+            self.assertEqual(get_days_of_power(1000, -2, 1000, 6, 1000, 6, 3000), -1)
+
+
+if __name__ == '__main__':
+    unittest.main()
